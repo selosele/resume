d01f3f5e76a7b72bf0d91d6751e3abe3fabf1405
selosele <osh3384@naver.com>
2023-06-23 17:08:25 +0900

    profile-image 컴포넌트 작업

---
+++ components/profile/profile-image.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex items-center overflow-hidden border-solid border-4 h-60">
+    <img :src="uiTextStore.uiText['profileImage']" alt="">
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUiTextStore } from '@/store/ui-text';
+
+const uiTextStore = useUiTextStore();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
