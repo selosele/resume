90e1cb8b8bbeeb5bdb8da325e816feff23d35ade
selosele <osh3384@naver.com>
2023-06-23 17:08:33 +0900

    profile-introduction 컴포넌트 작업

---
+++ components/profile/profile-introduction.vue
@@ -0,0 +1,23 @@
+<template>
+  <profile-bottom-group>
+    <profile-bottom-label-level-1>
+      {{ uiTextStore.uiText['introduction'].label }}
+    </profile-bottom-label-level-1>
+
+    <profile-bottom-text>
+      <p>
+        {{ uiTextStore.uiText['introduction'].text }}
+      </p>
+    </profile-bottom-text>
+  </profile-bottom-group>
+</template>
+
+<script setup lang="ts">
+import { useUiTextStore } from '@/store/ui-text';
+
+const uiTextStore = useUiTextStore();
+</script>
+
+<style lang="scss" scoped>
+
+</style>
