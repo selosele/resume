b2307f3c10dca8bfcb5c857e32f8a8736043a6df
selosele <osh3384@naver.com>
2023-06-23 17:08:43 +0900

    profile-skils 컴포넌트 작업

---
+++ components/profile/profile-skils-list-item.vue
@@ -0,0 +1,24 @@
+<template>
+  <profile-bottom-label-level-2>
+    {{ label }}
+  </profile-bottom-label-level-2>
+
+  <p class="mb-5">
+    {{ text.join(', ') }}
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    /** 라벨 */
+    label: String,
+    /** 내용 */
+    text: Array,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/profile/profile-skils.vue
@@ -0,0 +1,28 @@
+<template>
+  <profile-bottom-group>
+    <profile-bottom-label-level-1>
+      {{ uiTextStore.uiText['skils'].label }}
+    </profile-bottom-label-level-1>
+
+    <profile-bottom-text>
+      <profile-skils-list-item
+        v-for="(item,i) in list" :key="i"
+        :label="item.label"
+        :text="item.text"
+      />
+    </profile-bottom-text>
+  </profile-bottom-group>
+</template>
+
+<script setup lang="ts">
+import { useUiTextStore } from '@/store/ui-text';
+import { UiTextGroup } from '@/types/ui-text';
+
+const uiTextStore = useUiTextStore();
+
+const list = uiTextStore.uiText['skils'].text as UiTextGroup[];
+</script>
+
+<style lang="scss" scoped>
+
+</style>
