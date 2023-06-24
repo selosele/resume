05f3a1d0c4e1b7ed40a7ebc8452d9d8a78705e24
selosele <osh3384@naver.com>
2023-06-24 21:25:09 +0900

    ui-button 컴포넌트 작업

---
+++ components/ui/ui-button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    type="button"
+    :class="['text-slate-50', 'border-solid border border-transparent', 'transition',
+             { 'rounded-full': rounded }]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    /** 원 스타일 여부 */
+    rounded: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
