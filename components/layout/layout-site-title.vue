f2d0e22ca942c856ad798c1f3f321c09654a51a6
selosele <osh3384@naver.com>
2023-06-24 16:24:03 +0900

    layout-site-title 컴포넌트 작업

---
+++ components/layout/layout-site-title.vue
@@ -0,0 +1,16 @@
+<template>
+  <strong>{{ name }}</strong>
+</template>
+
+<script>
+export default {
+  props: {
+    /** 사이트 타이틀명 */
+    name: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
