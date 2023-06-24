e0c0c4b3aecaed338cbbccbdd495105ed8bc2fa6
selosele <osh3384@naver.com>
2023-06-24 16:23:33 +0900

    ui-icon-button 컴포넌트 작업

---
+++ components/ui/ui-icon-button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :type="type">
+    <ui-icon :name="icon" />
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    /** 버튼 Type */
+    type: {
+      type: String,
+      default: 'button'
+    },
+    /** 아이콘 클래스명 */
+    icon: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
