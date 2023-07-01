a11eb181814a8de8afbe78bb43c8b5edac4f2d47
selosele <osh3384@naver.com>
2023-07-01 17:58:18 +0900

    드롭다운 컴포넌트 작업

---
+++ components/ui/ui-dropdown-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <MenuButton :class="['inline-flex w-full justify-center gap-x-1.5 align-top',
+                         'rounded-md bg-white px-2 py-1 text-sm font-semibold text-gray-900',
+                         'shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50']"
+    >
+      {{ text }}
+      <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+    </MenuButton>
+  </div>
+</template>
+
+<script>
+import { MenuButton } from '@headlessui/vue';
+import { ChevronDownIcon } from '@heroicons/vue/20/solid';
+
+export default {
+  components: {
+    MenuButton,
+    ChevronDownIcon,
+  },
+  props: {
+    /** 드롭다운 텍스트 */
+    text: String,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/ui/ui-dropdown-list.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition
+    enter-active-class="transition ease-out duration-100"
+    enter-from-class="transform opacity-0 scale-95"
+    enter-to-class="transform opacity-100 scale-100"
+    leave-active-class="transition ease-in duration-75"
+    leave-from-class="transform opacity-100 scale-100"
+    leave-to-class="transform opacity-0 scale-95"
+  >
+    <MenuItems :class="['absolute right-0 z-10 mt-2 w-56',
+                        'origin-top-right rounded-md bg-white shadow-lg',
+                        'ring-1 ring-black ring-opacity-5 focus:outline-none']"
+    >
+      <div class="py-1">
+        <MenuItem
+          v-for="(item,i) in list" :key="i"
+          v-slot="{ active }">
+          <a
+            :href="item.href"
+            :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']"
+            :download="item.download ? '' : undefined"
+            @click="item.event"
+          >{{ item.text }} 
+          </a>
+        </MenuItem>
+      </div>
+    </MenuItems>
+  </transition>
+</template>
+
+<script>
+import { MenuItems, MenuItem } from '@headlessui/vue';
+
+export default {
+  components: {
+    MenuItems,
+    MenuItem,
+  },
+  props: {
+    /** 드롭다운 메뉴 목록 */
+    list: Array,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
