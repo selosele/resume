745a6edcb6b1c22e36a5ceb76e321555cc4fc046
selosele <osh3384@naver.com>
2023-07-02 18:29:46 +0900

    커스텀 분기점 변수 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': '380px'
+      },
       fontFamily: {
         sans: ['Noto Sans KR', 'sans-serif'],
       },
