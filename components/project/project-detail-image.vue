40db3922a4eed901bbbf0eb189dc3eb3d39077ad
selosele <osh3384@naver.com>
2023-12-24 13:37:02 +0900

    프로젝트 상세 이미지 고정 삭제

--- components/project/project-detail-image.vue
+++ components/project/project-detail-image.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="overflow-hidden w-full xl:min-h-[48.117647rem] xl:max-h-[53.529411rem] max-h-[300px] mx-auto mb-10 bg-slate-200">
+  <p class="overflow-hidden w-full mx-auto mb-10">
     <img :src="image" alt="">
   </p>
 </template>
@@ -13,5 +13,8 @@ defineProps({
 </script>
 
 <style lang="scss" scoped>
-
+// 이미지 높이값 고정 시 사용하는 클래스
+.h-fix {
+  @apply xl:min-h-[48.117647rem] xl:max-h-[53.529411rem] max-h-[300px] bg-slate-200;
+}
 </style>
