0d3833592629158e448b968848af278a12c44f72
selosele <osh3384@naver.com>
2024-06-22 12:47:13 +0900

    홈버튼 클릭이벤트 개선

--- components/layout/layout-mobile-header.vue
+++ components/layout/layout-mobile-header.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="xl:hidden block sticky z-10 top-0">
     <div class="flex justify-between bg-gray-200 px-4">
-      <NuxtLink
-        to="/"
-      >
+      <button type="button" @click="navigateHome">
         <ui-icon :name="'xi-home'" />
         <span class="sr-only">홈으로 가기</span>
-      </NuxtLink>
+      </button>
 
       <button type="button" @click="toggle">
         <ui-icon :name="isShow ? 'xi-close' : 'xi-bars'" />
@@ -23,12 +21,24 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+const route = useRoute();
+const router = useRouter();
+
 /** 목차 표시 여부 */
 let isShow = ref(false);
 
 /** 목차 토글 */
 const toggle = () => {
   isShow.value = !isShow.value;
+};
+
+/** 홈으로 이동 */
+const navigateHome = () => {
+  if (route.name === 'index') {
+    window.scrollTo(0, 0);
+    return;
+  }
+  router.push('/');
 };
 </script>
 
