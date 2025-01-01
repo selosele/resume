0fd189fccc1a8a7eb2440911a71ffc1838ada63f
selosele <osh3384@naver.com>
2025-01-01 12:00:09 +0900

    PDF 다운로드 기능 수정

--- components/layout/layout-footer.vue
+++ components/layout/layout-footer.vue
@@ -23,20 +23,20 @@ import html2canvas from 'html2canvas';
 import { useUiTextStore } from '@/store/ui-text';
 import { useUiLoadingStore } from '@/store/ui-loading';
 
-const route = useRoute();
 const uiTextStore = useUiTextStore();
 const uiLoadingStore = useUiLoadingStore();
 
 /** 화면을 PDF로 다운받는다. */
 const exportPdf = async () => {
-  const options = {
+  const canvasOptions = {
     allowTaint: true,
     useCORS: true,
-    scrollX: 0,
-    scrollY: 0,
-    windowWidth: getPdfWidth(),
     scale: 2,
     dpi: 300,
+    scrollX: 0,
+    scrollY: 0,
+    width: document.body.scrollWidth,   // 전체 화면 너비
+    height: document.body.scrollHeight, // 전체 화면 높이
     ignoreElements: (el: Element) => {
       const idList = ['btnExportPdf', 'btnGoList', 'detailBtns', 'layoutMobileHeader'];
       return idList.includes(el.id);
@@ -45,25 +45,28 @@ const exportPdf = async () => {
 
   uiLoadingStore.show();
 
-  const canvas = await html2canvas(document.body, options);
+  // 화면 전체 캡처
+  const canvas = await html2canvas(document.body, canvasOptions);
+
+  // PDF 크기 설정 (캔버스 크기 기반)
+  const pdfWidth = canvas.width * 0.264583;   // 픽셀 → mm (1px = 0.264583mm)
+  const pdfHeight = canvas.height * 0.264583; // 픽셀 → mm
+
+  // PDF 생성
   const imgData = canvas.toDataURL('image/png');
-  const pdf = new jsPDF();
-  
-  // 페이지 크기에 맞게 이미지 추가
-  pdf.addImage(imgData, 'PNG', 0, 0, pdf.internal.pageSize.width, pdf.internal.pageSize.height);
-  
-  // PDF 다운로드
+  const pdf = new jsPDF({
+    orientation: 'portrait', // 세로 방향
+    unit: 'mm', // 단위: mm
+    format: [pdfWidth, pdfHeight], // PDF 크기를 캔버스 크기에 맞춤
+  });
+
+  // PDF에 이미지 추가
+  pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+
+  // PDF 저장
   pdf.save(`${uiTextStore.json['title']}.pdf`);
 
   uiLoadingStore.hide();
-};
-
-/** 페이지별 PDF width값 반환 */
-const getPdfWidth = () => {
-  if (route.path.indexOf('/projects/') > -1) { // 프로젝트 상세조회 화면
-    return 1000;
-  }
-  return 1420;
 };
 </script>
 
